f9f7c8d68420b19a7a165092e0eca32546de9e4b
gabrielrportugal <gabrielrportugal@outlook.com>
2020-12-07 14:14:01 -0300

    starting rocketseat course about nextjs

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,8 @@
+import {useRouter} from 'next/router';
+
+export default function Product(){
+  //Forma de acessar parâmetro na url da aplicação
+  const router = useRouter();
+
+  return <h1>{router.query.slug}</h1>
+}
